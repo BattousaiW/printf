61fa661ea006f6cb59b3e1147adb51b2ee39bf10
Battousai <brandonwee08@gmail.com>
2023-07-27 20:08:37 +0200

    printf

--- _printf.c
+++ _printf.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
- * _printf - function my printf
- * @format: string whit format to print
+ * _printf - Outputs a formatted string.
+ * @format: Character string to print - may contain directives.
  *
- * Return: number of chars that print
+ * Return: The number of characters printed.
  */
 int _printf(const char *format, ...)
 {
+buffer_t *output;
 va_list args;
-int length = 0;
-if (format == NULL)
-return (-1);
-va_start(args, format);
-length = _print_format(format, args);
-va_end(args);
-return (length);
-}
+int ret;
 
+if (format == NULL)return (-1);
+output = init_buffer();
+if (output == NULL)return (-1);
+
+va_start(args, format);
+
+ret = run_printf(format, args, output);
+
+return (ret);
+}
